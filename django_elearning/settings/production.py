eca56cb7d993a614e24ba5d5ac1fec3bc9b0becf
Daniil Levchenko <monkaS123123@gmail.com>
2022-05-03 13:19:55 +0300

    Pre-deploy

--- django_elearning/settings/production.py
+++ django_elearning/settings/production.py
@@ -4,3 +4,14 @@ DEBUG = False
 ALLOWED_HOSTS = [
     "*",
 ]
+ADMINS = (
+    ('Daniel', 'monkaS123123@gmail.com')
+)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'elearning',
+        'USER': 'elearning',
+        'PASSWORD': '1111',
+    }
+}
